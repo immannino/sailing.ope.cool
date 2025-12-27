e96756d00a268ee41af5723f18d31340985ea17b
Tony Mannino <tonym@sb401k.com>
2025-12-26 16:04:06 -0800

    Fix typo

--- fetch.ts
+++ fetch.ts
@@ -9,7 +9,7 @@ try {
     const sailingData = { ...sailing, timestamp: new Date() }
     Deno.writeTextFileSync('./skills.json', JSON.stringify(res.skills))
     console.log('wrote files')
-    Deno.writeTextFileSync('./sailing.json', JSON.stringify(sailing))
+    Deno.writeTextFileSync('./sailing.json', JSON.stringify(sailingData))
     console.log('wrote files')
 } catch (err) {
     console.log(err)
